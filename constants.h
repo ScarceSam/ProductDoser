d3d20336b4636e9c5c76093932cd7678c965ab68
ScarceSam <tinker@sampearce.net>
2020-02-17 15:29:55 -0800

    ready for pull to master branch

--- constants.h
+++ constants.h
@@ -1,43 +1,40 @@
 //washer constants
-#define NUMBER_OF_WASHERS 4
-#define NUMBER_OF_PINS 4
+#define NUMBER_OF_WASHERS 
+#define NUMBER_OF_PINS 
 const uint8_t WASHER_COM_PINS[NUMBER_OF_WASHERS][NUMBER_OF_PINS] = 
-    {{1,2,3,4},
-     {2,3,4,5},
-     {3,4,5,6},
-     {4,5,6,7}
+    {
     };
 const uint8_t WASHER_VALVE[NUMBER_OF_WASHERS] = 
-    {23,34,54,65
+    {
     };
 const uint8_t WASHER_ID[NUMBER_OF_WASHERS] = 
-    {1,2,3,4
+    {
     };
 
 //Washer interupts
 //First dimension == enough room for up to all washers in queue,
 //second dimension == washer # and detergent #
-//volatile uint8_t DETERGENT_CALL_ISR_FLAG[NUMBER_OF_WASHERS][2] = {0};
-//static uint8_t DETERGENT_CALL_BUFFER[NUMBER_OF_WASHERS][2] = {0};
+volatile uint8_t DETERGENT_CALL_ISR_FLAG[NUMBER_OF_WASHERS][2] = {0};
+static uint8_t DETERGENT_CALL_BUFFER[NUMBER_OF_WASHERS][2] = {0};
 
 //detergent constants
-//const uint8_t NUMBER_OF_DETERGENTS = ;
-//const uint8_t DETERGENT_VALVE[NUBER_OF_DETERGENTS] = 
-//    {
-//    };
-//const uint8_t WATER_VALVE = ;
-//const uint8_t DRAIN_VALVE = ;
+const uint8_t NUMBER_OF_DETERGENTS = ;
+const uint8_t DETERGENT_VALVE[NUBER_OF_DETERGENTS] = 
+    {
+    };
+const uint8_t WATER_VALVE = ;
+const uint8_t DRAIN_VALVE = ;
 
 
 //pump constants
-//const uint8_t PUMP_PIN = ;
+const uint8_t PUMP_PIN = ;
 
 
 //display constants
 
 
 //flowmeter constants
-//const uint8_t FLOW_PIN = ;
+const uint8_t FLOW_PIN = ;
 
 //logging constants
 
--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include <stdint.h>
 #include "constants.h"
 #include "washer.h"
-//#include "detergent.h"
-//#include "pump.h"
-//#include "display.h"
-//#include "flowmeter.h"
-//#include "logging.h"
+#include "detergent.h"
+#include "pump.h"
+#include "display.h"
+#include "flowmeter.h"
+#include "logging.h"
 
 washer_t washer[NUMBER_OF_WASHERS];
 
@@ -22,18 +22,18 @@ void setup(void)
     // initialize washer interupts
 
     // initialize detergents
-//    for(int i = 0; i < NUMBER_OF_DETERGENTS; I++)
-//    {
-//        detergent_init(i, DETERGENT_VALVE[i]);
-//    }
+    for(int i = 0; i < NUMBER_OF_DETERGENTS; I++)
+    {
+        detergent_init(i, DETERGENT_VALVE[i]);
+    }
 
     // initialize pump
-//    pump_init(PUMP_PIN);
+    pump_init(PUMP_PIN);
 
     // initialize display
 
     // initialize flowmeter
-//    flow_init(FLOW_PIN);
+    flow_init(FLOW_PIN);
 
     // initialize logging
 }
@@ -42,13 +42,13 @@ void setup(void)
 int loop(void) // return 0 when not pumping
 {
     //return variable
-//    static uint8_t is_running = 0;
+    static uint8_t is_running = 0;
 
     //create washer queue
 
     //update washer queue
     //place any intupts into queue
-//    is_running = #of items in queue
+    is_running = #of items in queue
 
     //
 
