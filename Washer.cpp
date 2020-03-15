081ed8524634795be8d12252c6f3f1199e1f0ebe
ScarceSam <tinker@sampearce.net>
2020-03-15 12:29:04 -0700

    finish washer Struct and Initialization function

--- Washer.cpp
+++ Washer.cpp
@@ -1,10 +1,24 @@
 #include <Arduino.h>
 #include "Washer.h"
 
+washer_t washer[NUMBER_OF_WASHERS];
+
 void washer_init(void)
 {
-  for(int i; i < NUMBER_OF_WASHERS; i++)
+  for(int i = 0; i < NUMBER_OF_WASHERS; i++)
   {
     washer[i].number = i+1;
+    washer[i].i2cAddress = I2C_START_ADDRESS + (i/WASHERS_PER_EXPANSION);
+    for(int j = 0; j < NUMBER_OF_PINS; j++)
+    {
+      if(j < (NUMBER_OF_PINS - 1))
+      {
+        washer[i].com_pin[j] = (((i % NUMBER_OF_PINS) * NUMBER_OF_PINS) + j) % USED_PINS_PER_EXPANSION;
+      }
+      else
+      {
+        washer[i].valve_pin = (((i % NUMBER_OF_PINS) * NUMBER_OF_PINS) + j) % USED_PINS_PER_EXPANSION;
+      }
+    }
   }
 }
--- Washer.h
+++ Washer.h
@@ -3,13 +3,18 @@
 
 #include <Arduino.h>
 
-#define NUMBER_OF_PINS 4
+#define NUMBER_OF_PINS 5
 #define ID_LIMIT 16 
 #define NUMBER_OF_WASHERS 24
+#define I2C_START_ADDRESS 0x20
+#define INPUTS_PER_EXPANSION 15
+#define WASHERS_PER_EXPANSION (INPUTS_PER_EXPANSION / NUMBER_OF_PINS) 
+#define USED_PINS_PER_EXPANSION (WASHERS_PER_EXPANSION * NUMBER_OF_PINS)
+
 
 typedef struct {
   uint8_t number;
-  uint8_t i2cChannel;
+  uint8_t i2cAddress;
   uint8_t com_pin[NUMBER_OF_PINS];
   uint8_t valve_pin;
   uint8_t washerSize;
@@ -17,5 +22,7 @@ typedef struct {
 } washer_t;
 
 extern washer_t washer[NUMBER_OF_WASHERS];
+
+void washer_init(void);
 
 #endif
