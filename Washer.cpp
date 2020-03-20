b84dde04c8ca39e5c71c1847ef0eb0b3e533cc69
ScarceSam <tinker@sampearce.net>
2020-03-19 22:29:51 -0700

    Correct math error

--- Washer.cpp
+++ Washer.cpp
@@ -7,17 +7,25 @@ void washer_init(void)
 {
   for(int i = 0; i < NUMBER_OF_WASHERS; i++)
   {
+    //chronologically label the washers
     washer[i].number = i+1;
+
+    //Assign the GPIO expander I2C address
     washer[i].i2cAddress = I2C_START_ADDRESS + (i/WASHERS_PER_EXPANSION);
+
+    //Assign the GPIO expander pins
     for(int j = 0; j < NUMBER_OF_PINS; j++)
     {
+      //First assign the inputs from the washers
       if(j < (NUMBER_OF_PINS - 1))
       {
-        washer[i].com_pin[j] = (((i % NUMBER_OF_PINS) * NUMBER_OF_PINS) + j) % USED_PINS_PER_EXPANSION;
+        washer[i].com_pin[j] = (((i % USED_PINS_PER_EXPANSION) * NUMBER_OF_PINS) + j) % USED_PINS_PER_EXPANSION;
       }
+      
+      //The last pin is an output to the Washer valve
       else
       {
-        washer[i].valve_pin = (((i % NUMBER_OF_PINS) * NUMBER_OF_PINS) + j) % USED_PINS_PER_EXPANSION;
+        washer[i].valve_pin = (((i % USED_PINS_PER_EXPANSION) * NUMBER_OF_PINS) + j) % USED_PINS_PER_EXPANSION;
       }
     }
   }
