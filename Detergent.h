3f69cdf54d5f8df4027ffce36ea6e202a07e0ad8
ScarceSam <tinker@sampearce.net>
2020-03-20 20:07:56 -0700

    Add detergent struct

--- Detergent.h
+++ Detergent.h
@@ -3,6 +3,21 @@
 
 #include <Arduino.h>
 
+#define ID_LIMIT 16
+#define NUMBER_OF_DETERGENTS 4
+
+typedef struct{
+  uint8_t number;
+  uint8_t level_pin;
+  uint8_t flow_pin;
+  uint8_t valve_pin;
+  uint8_t ounces_per_pound;
+  uint8_t ounces_per_5minutes;
+  char detergent_name[ID_LIMIT];
+} detergent_t;
+
+extern detergent_t g_detergent[NUMBER_OF_DETERGENTS];
+
 void detergent_init(void);
 
 #endif
