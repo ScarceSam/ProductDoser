cf0064897c4cb5336a93e4cfcab6a6523d7779ef
ScarceSam <tinker@sampearce.net>
2020-03-20 23:07:40 -0700

    Initalize Detergent structs

--- Detergent.cpp
+++ Detergent.cpp
@@ -1,6 +1,22 @@
 #include <Arduino.h>
 #include "Detergent.h"
 
-void detergent_init(void){
-  
+const uint8_t LEVEL_PIN[] = {29, 11, 34, 33};
+const uint8_t FLOW_PIN[] = {16, 31, 17, 41};
+const uint8_t VALVE_PIN[] = {45, 2, 1, 0};
+
+detergent_t g_detergent[NUMBER_OF_DETERGENTS];
+
+void detergent_init(void)
+{
+  for(int i = 0; i < NUMBER_OF_DETERGENTS; i++)
+  {
+    //chronologically label the detergents
+    g_detergent[i].number = i+1;
+
+    //assign all the pins from const arrays
+    g_detergent[i].level_pin = LEVEL_PIN[i];
+    g_detergent[i].flow_pin = FLOW_PIN[i];
+    g_detergent[i].valve_pin = VALVE_PIN[i];
+  }
 }
