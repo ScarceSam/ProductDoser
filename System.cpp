a89880d486c29fe11cf4bd783b2635014a86fb2d
Sam Peace <tinker@sampearce.net>
2020-03-22 21:54:20 -0700

    Add pinout variables

--- System.cpp
+++ System.cpp
@@ -1,11 +1,15 @@
 #include <Arduino.h>
 #include "System.h"
 
-
+static const uint8_t pump_pin = 0;
+static const uint8_t drain_valve_pin = 0;
+static const uint8_t water_valve_pin = 0;
+static const uint8_t system_flow_sensor = 0;
+static uint8_t pump_running = 0;
 
 uint8_t system_idle(void)
 {
-  return 1;
+  return !pump_running;
 }
 
 void system_dose(washer_t washer, detergent_t detergent)
