f600f55faf577cd4b8f570582b7d6aec42059e0b
ScarceSam <tinker@sampearce.net>
2020-03-26 23:41:39 -0700

    Move system variables into a Struct

--- System.cpp
+++ System.cpp
@@ -1,11 +1,17 @@
 #include <Arduino.h>
 #include "System.h"
 
-static const uint8_t pump_pin = 0;
-static const uint8_t drain_valve_pin = 0;
-static const uint8_t water_valve_pin = 0;
-static const uint8_t system_flow_sensor = 0;
-static uint8_t pump_running = 0;
+
+struct system_info{
+  static const uint8_t pump_pin = 0;
+  static const uint8_t drain_valve_pin = 0;
+  static const uint8_t water_valve_pin = 0;
+  static const uint8_t system_flow_sensor = 0;
+  static uint8_t pump_running = 0;
+  static uint32_t pump_start_time = 0;
+}system_t;
+
+static system_t system_info;
 
 uint8_t system_idle(void)
 {
