58b255e59d4aec04829441047ba0b745a1e15281
ScarceSam <tinker@sampearce.net>
2020-03-27 00:20:13 -0700

    Fix struct definition

--- System.cpp
+++ System.cpp
@@ -2,20 +2,20 @@
 #include "System.h"
 
 
-struct system_info{
-  static const uint8_t pump_pin = 0;
-  static const uint8_t drain_valve_pin = 0;
-  static const uint8_t water_valve_pin = 0;
-  static const uint8_t system_flow_sensor = 0;
-  static uint8_t pump_running = 0;
-  static uint32_t pump_start_time = 0;
+typedef struct {
+  const uint8_t pump_pin = 0;
+  const uint8_t drain_valve_pin = 0;
+  const uint8_t water_valve_pin = 0;
+  const uint8_t system_flow_sensor = 0;
+  uint8_t pump_running = 0;
+  uint32_t pump_start_time = 0;
 }system_t;
 
 static system_t system_info;
 
 uint8_t system_idle(void)
 {
-  return !pump_running;
+  return !system_info.pump_running;
 }
 
 void system_dose(washer_t washer, detergent_t detergent)
