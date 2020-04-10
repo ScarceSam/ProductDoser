4425c89ee92bb47a00783e3a8ea72c007335a6c5
ScarceSam <tinker@sampearce.net>
2020-04-10 00:15:01 -0700

    Move struct and remove spaghetti function

--- Washer.cpp
+++ Washer.cpp
@@ -4,14 +4,25 @@
 #include "Adafruit_MCP23017.h"
 
 #define NUMBER_OF_WASHERS 24
+#define NUMBER_OF_COM_PINS 4
+#define NUMBER_OF_PINS (NUMBER_OF_COM_PINS + 1)
 #define I2C_START_ADDRESS 0
 #define INPUTS_PER_EXPANSION 16
 #define WASHERS_PER_EXPANSION (INPUTS_PER_EXPANSION / NUMBER_OF_PINS) 
 #define USED_PINS_PER_EXPANSION (WASHERS_PER_EXPANSION * NUMBER_OF_PINS)
 #define VALVE_OPEN HIGH
 
+typedef struct {
+  uint8_t number;
+  uint8_t i2cAddress;
+  uint8_t com_pin[NUMBER_OF_PINS];
+  uint8_t valve_pin;
+  uint8_t washer_size = 20;
+  char washer_id[ID_LIMIT];
+} washer_t;
+
 static washer_t washer[NUMBER_OF_WASHERS];
-static Adafruit_MCP23017 mcp[7];
+static Adafruit_MCP23017 mcp[8];
 
 void washer_init(void)
 {
@@ -63,11 +74,6 @@ void washer_get_next(uint8_t next_in_queue[])
   test_one = (test_one < 1) ? 4 : test_one; 
   next_in_queue[0] = test_zero;
   next_in_queue[1] = test_one;
-}
-
-washer_t washer_get_data(uint8_t washer_number)
-{
-  return washer[washer_number - 1];
 }
 
 void washer_open_valve(uint8_t washer_number)
--- Washer.h
+++ Washer.h
@@ -3,25 +3,13 @@
 
 #include <Arduino.h>
 
-#define NUMBER_OF_PINS 5
 #define ID_LIMIT 16 
-
-typedef struct {
-  uint8_t number;
-  uint8_t i2cAddress;
-  uint8_t com_pin[NUMBER_OF_PINS];
-  uint8_t valve_pin;
-  uint8_t washer_size = 20;
-  char washer_id[ID_LIMIT];
-} washer_t;
 
 void washer_init(void);
 
 uint8_t washer_ready(void);
 
 void washer_get_next(uint8_t next_in_queue[]);
-
-washer_t washer_get_data(uint8_t washer_number);
 
 void washer_open_valve(uint8_t washer_number);
 
