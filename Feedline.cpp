0c46c8b6f965ac7049e0c353fac019e1b6f2a697
ScarceSam <tinker@sampearce.net>
2020-06-07 17:11:27 -0700

    Clean up file

--- Feedline.cpp
+++ Feedline.cpp
@@ -2,7 +2,6 @@
 #include "Feedline.h"
 #include "Pinmap.h"
 
-
 typedef struct {
   const uint8_t COIL_A = FEEDLINE_PUMP_COIL_A_PIN;
   const uint8_t COIL_B = FEEDLINE_PUMP_COIL_B_PIN;
@@ -12,10 +11,10 @@ typedef struct {
   const uint8_t COIL_MAX_PULSE_PER_SEC = 5;
   const uint8_t PUMP_MAX_PULSE_PER_SEC = 5;
   const uint32_t PULSE_TIME_MILLI = 1000/((COIL_MAX_PULSE_PER_SEC <= PUMP_MAX_PULSE_PER_SEC) ? COIL_MAX_PULSE_PER_SEC : PUMP_MAX_PULSE_PER_SEC);
-  const uint8_t line_drain_valve_pin = FEEDLINE_END_DRAIN_VALVE_PIN;
-  const uint8_t water_valve_pin = FEEDLINE_WATER_VALVE_PIN;
-  const uint8_t feedline_flow_sensor = FEEDLINE_FLOW_PIN;
-  const uint8_t manifold_drain_valve_pin = FEEDLINE_MANIFOLD_DRAIN_VALVE_PIN;
+  const uint8_t LINE_DRAIN_VALVE_PIN = FEEDLINE_END_DRAIN_VALVE_PIN;
+  const uint8_t WATER_VAVLE_PIN = FEEDLINE_WATER_VALVE_PIN;
+  const uint8_t FEEDLINE_FLOW_SENSOR = FEEDLINE_FLOW_PIN;
+  const uint8_t MANIFOLD_DRAIN_VALVE_PIN = FEEDLINE_MANIFOLD_DRAIN_VALVE_PIN;
   uint8_t flush_oz = 10;
 }feedline_t;
 
@@ -25,9 +24,9 @@ void feedline_init(void)
 {
   pinMode(feedline_info.COIL_A, OUTPUT);
   pinMode(feedline_info.COIL_B, OUTPUT);
-  pinMode(feedline_info.line_drain_valve_pin, OUTPUT);
-  pinMode(feedline_info.water_valve_pin, OUTPUT);
-  pinMode(feedline_info.manifold_drain_valve_pin, OUTPUT);
+  pinMode(feedline_info.LINE_DRAIN_VALVE_PIN, OUTPUT);
+  pinMode(feedline_info.WATER_VAVLE_PIN, OUTPUT);
+  pinMode(feedline_info.MANIFOLD_DRAIN_VALVE_PIN, OUTPUT);
 }
 
 uint32_t feedline_pump_start(uint8_t volume_half_oz)
@@ -44,18 +43,18 @@ void feedline_valve(uint8_t valve, uint8_t state)
   switch(valve)
   {
     case WATER_VALVE:
-      digitalWrite(feedline_info.water_valve_pin, state);
+      digitalWrite(feedline_info.WATER_VAVLE_PIN, state);
       break;
     case LINE_DRAIN_VALVE:
-      digitalWrite(feedline_info.line_drain_valve_pin, state);
+      digitalWrite(feedline_info.LINE_DRAIN_VALVE_PIN, state);
       break;
     case MANIFOLD_DRAIN_VALVE:
-      digitalWrite(feedline_info.manifold_drain_valve_pin, state);
+      digitalWrite(feedline_info.MANIFOLD_DRAIN_VALVE_PIN, state);
       break;
     case ALL_VALVES:
-      digitalWrite(feedline_info.water_valve_pin, state);
-      digitalWrite(feedline_info.line_drain_valve_pin, state);
-      digitalWrite(feedline_info.manifold_drain_valve_pin, state);
+      digitalWrite(feedline_info.WATER_VAVLE_PIN, state);
+      digitalWrite(feedline_info.LINE_DRAIN_VALVE_PIN, state);
+      digitalWrite(feedline_info.MANIFOLD_DRAIN_VALVE_PIN, state);
   }
 }
 
--- Feedline.h
+++ Feedline.h
@@ -1,5 +1,4 @@
-#ifndef Feedline_h
-#define Feedline_h
+#pragma once
 
 #include <Arduino.h>
 #include "Washer.h"
@@ -14,18 +13,16 @@ enum valves{ LINE_DRAIN_VALVE, WATER_VALVE, MANIFOLD_DRAIN_VALVE, ALL_VALVES };
 
 void feedline_init(void);
 
+void feedline_update(void);
+
 uint32_t feedline_pump_start(uint8_t volume_oz);
-
-void feedline_valve(uint8_t valve, uint8_t state);
-
-uint32_t feedline_flush_time_milli(void);
 
 void pulse_pump(void);
 
-uint8_t feedline_flush_oz(void);
-
-void feedline_update(void);
-
 bool feedline_pumping(void);
 
-#endif
+void feedline_valve(uint8_t valve, uint8_t state);
+
+uint8_t feedline_flush_oz(void);
+
+bool feedline_pumping(void);
