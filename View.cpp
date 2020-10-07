ace58914b2cce7af8de4b73cfd7bff1bae28ca17
Sam Peace <tinker@sampearce.net>
2020-10-06 23:53:09 -0700

    update step line when displaying state

--- View.cpp
+++ View.cpp
@@ -1,6 +1,7 @@
 #include "View.h"
 #include <LiquidCrystalFast.h>
 #include "Pinmap.h"
+#include "State.h"
 
 #define DISPLAY_X 20
 #define DISPLAY_Y 4
@@ -13,6 +14,8 @@ char* stepNames[4];
 
 void shiftUp(void);
 void copyToLine(char* newMessage, char* oldMessage);
+void concatinate(char* str1, char* str2, char* str3, uint8_t sizeLimit);
+uint8_t charLen(char* string);
 
 void view_init(void)
 {
@@ -100,9 +103,71 @@ void view_clear(void)
 
 void view_display_state(void)
 {
-  copyToLine("Washer: ", line[0]);
-  copyToLine("Product: ", line[1]);
-  copyToLine("Step: ", line[2]);
-  copyToLine("test", line[3]);
+  char scratch[DISPLAY_X + 1];
+
+  //display line 1
+  copyToLine("Wshr: ", line[0]);
+
+  //display line 2
+  copyToLine("Prod: ", line[1]);
+
+  //display line 3
+  concatinate("Step: ", stepNames[state_currentState()], scratch, 20);
+  copyToLine(scratch, line[2]);
+
+  //display line 4
+  copyToLine("Time:", line[3]);
   updateScreen();
+}
+
+void concatinate(char* str1, char* str2, char* str3, uint8_t sizeLimit)
+{
+  if (sizeLimit > DISPLAY_X)
+  {
+    sizeLimit = DISPLAY_X;
+  }
+
+  uint8_t endLoop = sizeLimit;
+  uint8_t strOneLen = charLen(str1);
+
+  for (int i = 0; i <= endLoop; i++)
+  {
+    //if iteration is less than end of string one
+    if (i < strOneLen)
+    {
+      //copy charater from 1 to returnstring
+      str3[i] = str1[i];
+    }
+    //if past string one end of line
+    else if (i >= strOneLen)
+    {
+      //copy character form str2 to returnstring
+      str3[i] = str2[i - strOneLen];
+    }
+
+    //if last iteration
+    if (i == endLoop)
+    {
+      //end line
+      str3[i] = '\0';
+    }
+
+    //if return string EOL
+    if (str3[i] == '\0')
+    {
+      //finish iteration
+      i = endLoop;
+    }
+  }
+}
+
+uint8_t charLen(char* string)
+{
+  for (int i = 0; i < (DISPLAY_X + 1);i++)
+  {
+    if( string[i] == '\0')
+    {
+      return i;
+    }
+  }
 }
