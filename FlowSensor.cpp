4009747797c864f3676ce8b51cc4c909aede0a30
Sam Peace <tinker@sampearce.net>
2020-11-15 11:37:07 -0800

    show display notification if flow sensor is ignored

--- FlowSensor.cpp
+++ FlowSensor.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include "Pinmap.h"
+#include "View.h"
 
-static bool ignore_flowsensor = false;
+static bool ignore_flowsensor = true;
+static bool ignore_notice = false;
 static volatile uint32_t pulse_time[256] = {0};
 static volatile uint8_t time_cursor = 0;
 static uint16_t max_pulse_delta = 750;
@@ -24,7 +26,14 @@ bool flowsensor_is_flowing(void)
   b_return_value = (uint32_t)(millis() - pulse_time[time_cursor - 1] < max_pulse_delta);
 
   if(ignore_flowsensor)
+  {
       b_return_value = true;
+      if(!ignore_notice)
+      {
+        view_println("Flow Sensor - OFF");
+        ignore_notice = true;
+      }
+  }
 
   return b_return_value;
 }
