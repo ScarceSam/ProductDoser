46e2de21a378c39c765071843a67b5367d7ff240
Sam Peace <tinker@sampearce.net>
2020-11-15 16:09:57 -0800

    remove vestigial code

--- SDcard.cpp
+++ SDcard.cpp
@@ -10,8 +10,6 @@ static File saveFile;
 
 uint32_t find_device_info(String device);
 uint32_t find_setting_info(uint32_t start, String setting);
-String fetch_setting(uint32_t start);
-String clean_setting(String value);
 void remove_characters(char phrase[]);
 
 uint8_t fetch_setting(uint32_t start, char* string);
@@ -26,43 +24,6 @@ uint8_t sdcard_init(void)
   }
 
   return return_value;
-}
-
-String SDcard_read_string(String device, String setting)
-{
-  String return_string;
-  uint32_t location_in_file = 0;
-
-  location_in_file = find_device_info(device);
-
-  if(location_in_file > 0)
-    location_in_file = find_setting_info(location_in_file, setting);
-
-  if(location_in_file == 0)
-  {
-    return_string =  "\0";
-  }
-  else
-  {
-    return_string = fetch_setting(location_in_file);
-    return_string = clean_setting(return_string);
-  }
-
-  return return_string;
-}
-
-int32_t SDcard_read_int(String device, String setting)
-{
-  String value = SDcard_read_string(device, setting); 
-
-  int32_t number = -1;
-
-  if('\0' != value[0])
-  {
-    number = value.toInt();
-  }
-
-  return number;
 }
 
 uint32_t find_device_info(String device)
@@ -181,63 +142,6 @@ uint32_t find_setting_info(uint32_t start, String setting)
     return_value = 0;
 
   return return_value;
-}
-
-String fetch_setting(uint32_t start)
-{
-  saveFile = SD.open("settings.txt", FILE_READ);
-  char working_array[60];
-  saveFile.seek(start);
-
-  for(int i = 0; i < 60; i++)
-  {
-    if(saveFile.available())
-    {
-      working_array[i] = saveFile.read();
-
-      if (working_array[i] == '\n')
-      {
-        i = 60;
-      }
-    }
-  }
-    
-  saveFile.close();
-
-  String return_string = working_array;
-  return return_string;
-}
-
-String clean_setting(String input)
-{
-  char working_array[60];
-  input.toCharArray(working_array, 60);
-
-  bool copying = 1;
-  int in_cursor, out_cursor = 0;
-
-  while(copying)
-  {
-    working_array[out_cursor] = working_array[in_cursor];
- 
-    if(out_cursor == 0 && working_array[in_cursor] == ' ')
-    {
-      in_cursor++;
-    }
-    else if(working_array[in_cursor] == '\n')
-    {
-      copying = 0;
-      working_array[out_cursor] = '\0';
-    }
-    else
-    {
-      in_cursor++;
-      out_cursor++;
-    }
-  }  
-  
-  String return_string = working_array;
-  return return_string;
 }
 
 void remove_characters(char phrase[])
--- SDcard.h
+++ SDcard.h
@@ -2,10 +2,6 @@
 
 uint8_t sdcard_init(void);
 
-String SDcard_read_string(String device, String setting);
-
 uint8_t SDcard_read_string(const char* device, const char* setting, char* result, uint8_t max_len);
-
-int32_t SDcard_read_int(String device, String setting);
 
 uint8_t SDcard_read_int(const char* device, const char* setting, uint8_t* result);
