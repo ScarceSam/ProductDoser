c2d88f58c978e00b854838df48c0cb378c77d523
Sam Peace <tinker@sampearce.net>
2020-11-21 23:49:31 -0800

    correct failing wrap around and move millis() out of interrupt

--- FlowSensor.cpp
+++ FlowSensor.cpp
@@ -7,23 +7,33 @@ static bool ignore_notice = false;
 static volatile uint32_t pulse_time[256] = {0};
 static volatile uint8_t time_cursor = 0;
 static uint16_t max_pulse_delta = 750;
+static volatile bool save_pulse = false;
 
-static void increment_count()
+static void add_pulse()
 {
-    ++time_cursor;
-    pulse_time[time_cursor] = millis();
+  save_pulse = true;
 }
 
 void flowsensor_init(void)
 {
-  attachInterrupt(digitalPinToInterrupt(FLOWSENSOR_PIN), increment_count, CHANGE);
+  pinMode(FLOWSENSOR_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(FLOWSENSOR_PIN), add_pulse, CHANGE);
 }
 
 bool flowsensor_is_flowing(void)
 {
   bool b_return_value = false;
 
-  b_return_value = (uint32_t)(millis() - pulse_time[time_cursor - 1] < max_pulse_delta);
+  if(save_pulse)
+  {
+    pulse_time[time_cursor] = millis();
+    ++time_cursor;
+    save_pulse = false;
+  }
+
+  uint32_t test_value = pulse_time[(uint8_t)(time_cursor - 40)];
+
+  b_return_value = (uint32_t)(millis() - test_value < max_pulse_delta);
 
   if(ignore_flowsensor)
   {
