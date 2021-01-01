ced8ee1e515929cb4ed45c3724012896f5b8a946
Sam Peace <tinker@sampearce.net>
2020-12-31 23:36:13 -0800

    only check for button pushes if there has been a change via interrupt

--- Buttons.cpp
+++ Buttons.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "Buttons.h"
+#include "Pinmap.h"
 #include <Wire.h>
 #include <SparkFunSX1509.h>
 
@@ -8,41 +9,48 @@ SX1509 expander;
 const int BUTTONS[] = {0, 1, 2, 3, 4};
 const int BUTTON_COUNT = (sizeof(BUTTONS)/sizeof(BUTTONS[0]));
 const int DEBOUNCE = 64;
+const int INTERRUPT_PIN = SX1509_INT_PIN;
 
 void buttons_init(void)
 {
   expander.begin(0x3E);
-  
+
   for(int i = 0; i < BUTTON_COUNT; i++)
   {
     expander.pinMode(BUTTONS[i], INPUT_PULLUP);
     expander.enableInterrupt(BUTTONS[i], CHANGE);
   }
+
   expander.debounceTime(DEBOUNCE);
   for(int i = 0; i < BUTTON_COUNT; i++)
   {
     expander.debouncePin(BUTTONS[i]);
   }
+
+  pinMode(INTERRUPT_PIN, INPUT_PULLUP);
 }
 
 int buttons_pushed(void)
 {
   static int last_pushed = 0;
   int pushed = 0;
-  
-  for(int i = 0; i < BUTTON_COUNT; i++)
+
+  if(digitalRead(INTERRUPT_PIN) == 0)
   {
-    if(expander.digitalRead(BUTTONS[i]) == LOW)
+    for(int i = 0; i < BUTTON_COUNT; i++)
     {
-      pushed |= (1<<i);
+      if(expander.digitalRead(BUTTONS[i]) == LOW)
+      {
+        pushed |= (1<<i);
+      }
+      else
+      {
+        last_pushed &= ~(1<<i);
+      }
     }
-    else
-    {
-      last_pushed &= ~(1<<i);
-    }
+    pushed = pushed - last_pushed;
+    last_pushed += pushed;
   }
 
-  pushed = pushed - last_pushed;
-  last_pushed += pushed;
   return pushed;
 }
