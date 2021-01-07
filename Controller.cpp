a979582dc24c93c887edbcd163d9f9733485f245
Sam Peace <tinker@sampearce.net>
2021-01-06 23:09:41 -0800

    add the beginnings of the menu navigation code

--- Controller.cpp
+++ Controller.cpp
@@ -1,9 +1,43 @@
 #include <Arduino.h>
 #include "View.h"
 #include "State.h"
+#include "Buttons.h"
+
+void display_state(void);
 
 void controller_update_screen(void)
 {
+  static bool in_menu = false;
+  static long interaction_at = 0;
+  static long menu_timeout = 5000;
+
+  int nav_buttons = buttons_pushed();
+  if(nav_buttons)
+  {
+    interaction_at = millis();
+  }
+
+  if(in_menu)
+  {
+    if((millis() - interaction_at) > menu_timeout)
+    {
+      in_menu = false;
+    }
+  }
+  else if(nav_buttons & (1<<3))
+  {
+    view_clear();
+    view_println("        MENU        ");
+    view_println("//under construction");
+    in_menu = true;
+  }
+  else
+  {
+    display_state();
+  }
+}
+
+void display_state(void){
   static uint8_t previous_state = 0;
   static uint16_t previous_sec = 0;
 
