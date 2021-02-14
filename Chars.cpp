25b5de07cd13f2d2bc467efa805c44924e6a8e45
Sam Peace <tinker@sampearce.net>
2021-02-13 21:22:43 -0800

    move concat function to allow wider use

--- Chars.cpp
+++ Chars.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "Chars.h"
 
 void clear_char_array(char* string, uint8_t max_len)
 {
@@ -53,4 +54,54 @@ void char_append_digits(char* string, uint8_t number, uint8_t max_len)
     string[string_end] = char_number;
     string[string_end + 1] = '\0';
   }
+}
+
+void char_concatenate(char* output, const char* input1, const char* input2, uint8_t sizeLimit)
+{
+  uint8_t endLoop = sizeLimit;
+  uint8_t strOneLen = char_charLen(input1);
+
+  for (int i = 0; i < endLoop; i++)
+  {
+    //if iteration is less than end of string one
+    if (i < strOneLen)
+    {
+      //copy charater from 1 to returnstring
+      output[i] = input1[i];
+    }
+    //if past string one end of line
+    else if (i >= strOneLen)
+    {
+      //copy character form str2 to returnstring
+      output[i] = input2[i - strOneLen];
+    }
+
+    //if last iteration
+    if (i == (endLoop - 1))
+    {
+      //end line
+      output[i] = '\0';
+    }
+
+    //if return string EOL
+    if (output[i] == '\0')
+    {
+      //finish iteration
+      i = endLoop;
+    }
+  }
+}
+
+uint8_t char_charLen(const char* string)
+{
+  uint8_t return_value = 0;
+  for (int i = 0; i < (20 + 1);i++)
+  {
+    if( string[i] == '\0')
+    {
+      return_value = i;
+      break;
+    }
+  }
+  return return_value;
 }
--- Chars.h
+++ Chars.h
@@ -6,3 +6,7 @@ void clear_char_array(char* string, uint8_t max_len);
 void copy_char_array(char* copy_array, const char* master_array, uint8_t max_len);
 
 void char_append_digits(char* string, uint8_t number, uint8_t max_len);
+
+void char_concatenate(char* output, const char* input1, const char* input2, uint8_t sizeLimit);
+
+uint8_t char_charLen(const char* string);
