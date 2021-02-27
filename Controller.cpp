a514885a0bc2cba5c1f889cb35d30d2b3c1651f5
Sam Peace <tinker@sampearce.net>
2021-02-27 12:50:08 -0800

    add function access to the menu

--- Controller.cpp
+++ Controller.cpp
@@ -9,6 +9,7 @@ void display_state(void);
 bool display_menu(int buttons_pressed);
 void change_menu_position(int* menu_location, int* menu_selection, int button_pressed);
 void assemble_menu_text(char displaied_text[4][21], int menu_location, int menu_selection, int buttons_pressed);
+int assemble_func_text(char text[4][21], int menu_location, int menu_selection, int* buttons_pressed);
 
 void controller_update_screen(void)
 {
@@ -52,50 +53,87 @@ void display_state(void){
 
 bool display_menu(int buttons_pressed)
 {
-  bool return_value = false;
+  static bool in_menu = false;
   static int menu_location = -1;
   static int menu_selection = 0;
   static long interaction_at = 0;
   static long menu_timeout = 5000;
+  static bool in_function = false;
+  static long callback_time = 0;
+  static long callback_set = 0;
+  static char menu_text[4][21];
+  bool callback_flag = false;
 
   if(buttons_pressed)
   {
     interaction_at = millis();
   }
-  else if((millis() - interaction_at) > menu_timeout)
+  else if(!in_function && (millis() - interaction_at) > menu_timeout)
   {
     menu_location = -1;
+    in_menu = false;
   }
 
-  change_menu_position(&menu_location, &menu_selection, buttons_pressed);
-
-  if((menu_location >= MENU_ROOT) && buttons_pressed)
+  if(in_function && callback_time && ((millis() - callback_set) > callback_time))
   {
-    char displaied_text[4][21];
+    callback_flag = true;
+  }
+  else
+  {
+    callback_flag = false;
+  }
+
+  if(callback_flag || buttons_pressed)
+  {
     for(int i = 0; i < 4; i++)
     {
-      clear_char_array(displaied_text[i], 21);
+      clear_char_array(menu_text[i], 21);
     }
 
-    if(menu_is_function(menu_location))
+    if(!in_function)
     {
-      char_concatenate(displaied_text[0], "<", menu_get_name(menu_location), 21);
-      char_concatenate(displaied_text[0], displaied_text[0], ">", 21);
+      change_menu_position(&menu_location, &menu_selection, buttons_pressed);
+    }
+
+    if((menu_location >= MENU_ROOT) && (menu_is_function(menu_location)))
+    {
+      callback_time = assemble_func_text(menu_text, menu_location, menu_selection, &buttons_pressed);
+      callback_set = millis();
+      in_function = true;
+
+      if(buttons_pressed & BUTTON_LEFT)
+      {
+        change_menu_position(&menu_location, &menu_selection, buttons_pressed);
+        in_function = false;
+      }
+    }
+
+    if((menu_location >= MENU_ROOT) && (!in_function))
+    {
+      for(int i = 0; i < 4; i++)
+      {
+        clear_char_array(menu_text[i], 21);
+      }
+      assemble_menu_text(menu_text, menu_location, menu_selection, buttons_pressed);
+      in_menu = true;
     }
     else
     {
-      assemble_menu_text(displaied_text, menu_location, menu_selection, buttons_pressed);
+      in_menu = false;
     }
 
-    view_clear();
-    for(int i = 0; i < 4; i++)
+    if(in_menu || in_function)
     {
-      view_println(displaied_text[i]);
+      view_clear();
+      for(int i = 0; i < 4; i++)
+      {
+        view_println(menu_text[i]);
+      }
     }
   }
 
-  (menu_location >= MENU_ROOT) ? (return_value = true) : (return_value = false);
-  return return_value;
+  //(menu_location >= MENU_ROOT) ? (return_value = true) : (return_value = false);
+  return (in_menu || in_function);//return_value;
 }
 
 void change_menu_position(int* menu_location, int* menu_selection, int buttons_pressed)
@@ -169,7 +207,7 @@ void assemble_menu_text(char displaied_text[4][21], int menu_location, int menu_
 
   for(int i = 1; i < 4; i++)
   {
-    if(displayed_menu == menu_selection)
+    if(displayed_menu == menu_selection)// && displayed_menu != 0)
     {
       char_concatenate(displaied_text[i], ">", menu_get_name(displayed_menu), 21);
     }
@@ -180,4 +218,15 @@ void assemble_menu_text(char displaied_text[4][21], int menu_location, int menu_
 
     displayed_menu = menu_get_next_sibling(displayed_menu);
   }
+}
+
+int assemble_func_text(char text[4][21], int menu_location, int menu_selection, int* buttons_pressed)
+{
+      static int return_value = 0;
+      char_concatenate(text[0], "<", menu_get_name(menu_location), 21);
+      char_concatenate(text[0], text[0], ">", 21);
+      copy_char_array(text[2], "function = nullptr", 21);
+      menu_function(menu_location, text, buttons_pressed);
+
+      return return_value;
 }
